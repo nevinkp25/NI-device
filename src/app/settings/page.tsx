b6915830db048534ceb298eb10e7ff4cb5eb9ea6
Nevin Alcober <nevin@emenu.net>
2026-06-26 11:51:31 +0000

    Instead of End Shift make it Logout Please! (_for element <Button>_)

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -80,7 +80,7 @@ export default function SettingsPage() {
       <footer className="p-4">
           <Button variant="outline" className="w-full" onClick={handleLogout}>
               <LogOut className="mr-2 h-4 w-4" />
-              End Shift
+              Logout
           </Button>
       </footer>
     </div>
